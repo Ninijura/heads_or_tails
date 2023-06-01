9ae5e1ced71bbc02237ba613c62daa4e185b3755
Kira Kutscher <kira@tailorsoft.de>
2023-06-01 09:34:20 +0200

    added individual prompts for each round of input and added some amount of documentation. Also the function now returns the pair of heads and tails to be able to continue processing with them.

--- hot.ml
+++ hot.ml
@@ -1,25 +1,31 @@
-let print_results (n_heads, n_tails) =
-  print_string ("You rolled " ^ string_of_int n_heads ^ " heads and " ^  string_of_int n_tails ^ " tails.");;
 
+(* function to finish up the process by printing out how your results are distributed and returning the pair of (number_of_heads, number_of_tails) as result. *)
+let print_results (n_heads, n_tails) =
+  let () = print_string ("You rolled " ^ string_of_int n_heads ^ " heads and " ^  string_of_int n_tails ^ " tails.")
+  in (n_heads, n_tails);;
+
+
+(* main counting function that repeatedly asks the user to input the result of their throw. *)
 let start_counting (heads, tails, safeword) =
-  let rec count (n_heads, n_tails) =
-    let line = read_line ()
+  let rec count (n_heads, n_tails, n_throws) =
+    let () = print_string ("Enter the result of throw #" ^ string_of_int n_throws ^ ": ")
+    and line = read_line ()
     in if line = heads
-       then count (n_heads + 1, n_tails)
+       then count (n_heads + 1, n_tails, n_throws + 1)
        else if line = tails
-       then count (n_heads, n_tails + 1)
+       then count (n_heads, n_tails + 1, n_throws + 1)
        else if line = safeword
        then print_results (n_heads, n_tails)
-       else print_string ("I'm sorry, I don't understand your input. Your commands are either " ^ heads ^ ", " ^ tails ^ ", or " ^ safeword ^ " to exit.\n");
-       count (n_heads, n_tails)
-  in count (0,0);;
+       else let () = print_string ("I'm sorry, I don't understand your input. Your commands are either " ^ heads ^ ", " ^ tails ^ ", or " ^ safeword ^ " to exit.\n")
+            in count (n_heads, n_tails, n_throws)
+  in count (0,0,1);;
     
 
 
 
-(* initialisation function that asks for the keybindings for heads, tails and exit and calls the counting function. *)
+(* main function that asks for the keybindings for heads, tails and exit and calls the counting function. *)
 
-let init () =
+let count_my_rolls () =
   print_string "Hi what do you call the first side of your coin?\n";
   let side_one = read_line ()
   in print_string "What do you call the other side of your coin?\n";
